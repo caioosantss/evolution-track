2f313e4c81d57fe9df97e8ecdfb2fe3057b24f13
caioosantss <gilkamesh8@gmail.com>
2026-04-16 22:52:09 -0300

    feat: quebra de metodos
    - adicionado método verificar_artista
    - adicionado método verificar_musica
    - adicionado método Incluir_musica

--- POO/Ex1-Register/main.py
+++ POO/Ex1-Register/main.py
@@ -4,6 +4,7 @@ class Registro:
     Artistas = []
     idArtistas = 0
     musicas = {}
+
     def __init__(self,artista,Musica,idMusica):
         self.artista = artista
         self.idartista = Registro.idArtistas
@@ -16,16 +17,18 @@ class Registro:
         f"o artista {self.artista} é o artista numero {self.idartista} e possui as seguintes músicas em sua discografia {self.discografia}"
         )
 
-    @classmethod
-    def Verificar_musica (self,musica):
-        if musica not in Registro.Artistas:
+    def Verifica_musica (self, musica):
+        if musica not in Registro.musicas:
             return True
         else:
-            return False
+            return False 
 
+    def Incluir_Musica(self,musica):
+        if musica not in self.discografia:
+            self.discografia = musica
 
     @classmethod    
-    def Verifica_artista (self, artista):
+    def Verifica_artista (cls, artista):
         if artista not in Registro.Artistas:
             return True
         else:
@@ -35,5 +38,27 @@ class Registro:
     @classmethod
     def Registrar_artista(cls):
         #inicia fabrica de objetos
-            a
-        
+
+            #registra o artista e uma musica
+            nome = input("digite o nome do artista ")
+            if Registro.Verifica_artista(nome) is True:
+                artista = nome
+
+                #inicia cadastro de musica
+                musica = input("digite uma musica ")
+                
+                if Registro.Verifica_musica(musica) is True:
+                    
+                    Registro.Incluir_Musica(musica)
+                    return(artista)
+
+                else:
+  
+                   return('musica ja incluida na discografia do artista')
+            else:
+                #incluir aqui uma forma de introduzir o ID do artista
+                return("artista já registrado")
+
+    def Incluir_Musica(self,musica):
+        if musica not in self.discografia:
+            self.discografia = musica
