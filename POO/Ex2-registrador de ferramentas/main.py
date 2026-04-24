1222f5938ff423aead6b2be61964292f160201b7
caioosants <gilkamesh8@gmail.com>
2026-04-24 15:36:31 -0300

    update

--- POO/Ex2-registrador de ferramentas/main.py
+++ POO/Ex2-registrador de ferramentas/main.py
@@ -1,18 +1,27 @@
 class registrador():
 
     ferramentas = {}
-    funcionarios = []
+    funcionarios = {}
     setores = []
-
-    def Registrar_setor(self):
+    
+    @classmethod
+    def Registrar_setor(cls):
         setor = input("qual setor deseja registrar? ")
         registrador.setores.append(setor)  
         return (f"setor {setor} registrado")                                
 
     @classmethod
-    def cria_ferramenta(cls):
+    def criar_ferramenta(cls):
         print("deseja criar uma ferramenta a qual setor? ")
         print(registrador.setores)
+        #incluir uma chamada a classe de ativos para criação da ferramenta
+     
+    @classmethod   
+    def verificar_responsavel(cls):
+        print(f"qual responsavel deseja desginar? ")
+        print(registrador.funcionarios)
+        #montar lógica de case para retornar apenas funcionarios que ja existam em registrador
+
 
 class ativo():
     def __init__(self,nome,setor):
@@ -23,17 +32,12 @@ class ativo():
 
 class mala ():
     def __init__(self, ferramental):
-        self.responsavel = mala.verificar_responsavel()
-        self.ferramental = mala.extrair_ferramentas()
-
-    def verificar_responsavel(self):
-        print(f"qual responsavel deseja desginar? ")
-        print(registrador.funcionarios)
-        #montar lógica de case para retornar funcionarios
+        self.responsavel = registrador.verificar_responsavel()
+        self.ferramental = ferramental
 
     def extrair_ferramentas(self):
         #montar lógica apra extrair lista de mala de ferramentas
-        #montar lógica de registro individual de ferramentas pela própria lógica a partir da classe ativos
-        pass
+        #montar lógica consistente pois extrair ferramentas sempre será chamado para a criação de uma mala de ferramentas
+        pass    
 
     
